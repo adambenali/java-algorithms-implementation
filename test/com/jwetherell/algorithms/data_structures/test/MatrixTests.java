c86ed6a0bf19fecb2ae9eca1c35ad7b04bf17dcc
Gabriel <gc.chang95@gmail.com>
2020-02-19 10:25:38 +0100

    fix and split the compare test
    Fixed the testNumericTypesCompare1 and split it to an additional test.

--- test/com/jwetherell/algorithms/data_structures/test/MatrixTests.java
+++ test/com/jwetherell/algorithms/data_structures/test/MatrixTests.java
@@ -115,11 +115,11 @@ public class MatrixTests {
 
     /**
      * Testing the compare function in Matrix.java
-     * by testing BigDecimal and BigInteger numeric type for the matrices.
+     * by testing BigDecimal numeric type for the matrices.
      * This test should return true.
      */
     @Test
-    public void testNumericTypesCompare1() {
+    public void testBigDecimalCompare() {
         // Testing BigDecimal branch
         Matrix<BigDecimal> matrix1 = new Matrix<BigDecimal>(2, 2);
         matrix1.set(0, 0, new BigDecimal("4.0"));
@@ -150,50 +150,30 @@ public class MatrixTests {
 
         assertTrue(matrix3.equals(matrix4));
 
-        // Testing Long branch by making o2 instanceof Long true
-        Matrix<Integer> matrix5 = new Matrix<Integer>(2, 2);
-        matrix5.set(0, 0, 4);
-        matrix5.set(0, 1, 9);
-        matrix5.set(1, 0, 3);
-        matrix5.set(1, 1, 2);
+    }
 
-        Matrix<Long> matrix6 = new Matrix<Long>(2, 2);
-        matrix6.set(0, 0, 4L);
-        matrix6.set(0, 1, 9L);
-        matrix6.set(1, 0, 3L);
-        matrix6.set(1, 1, 2L);
+    /**
+     * Testing the compare function in Matrix.java
+     * by testing BigInterger numeric type for the matrices.
+     * This test should return true.
+     */
+    @Test
+    public void testBigIntegerCompare() {
+        // Testing BigInteger branch
+        Matrix<BigInteger> matrix3 = new Matrix<BigInteger>(2, 2);
+        matrix3.set(0, 0, new BigInteger("4"));
+        matrix3.set(0, 1, new BigInteger("9"));
+        matrix3.set(1, 0, new BigInteger("3"));
+        matrix3.set(1, 1, new BigInteger("2"));
 
-        assertTrue(matrix5.equals(matrix6));
+        Matrix<BigInteger> matrix4 = new Matrix<BigInteger>(2, 2);
+        matrix4.set(0, 0, new BigInteger("4"));
+        matrix4.set(0, 1, new BigInteger("9"));
+        matrix4.set(1, 0, new BigInteger("3"));
+        matrix4.set(1, 1, new BigInteger("2"));
 
-        // Testing Double branch
-        Matrix<Double> matrix7 = new Matrix<Double>(2, 2);
-        matrix7.set(0, 0, 4.0d);
-        matrix7.set(0, 1, 9.0d);
-        matrix7.set(1, 0, 3.0d);
-        matrix7.set(1, 1, 2.0d);
+        assertTrue(matrix3.equals(matrix4));
 
-        Matrix<Double> matrix8 = new Matrix<Double>(2, 2);
-        matrix8.set(0, 0, 4.0d);
-        matrix8.set(0, 1, 9.0d);
-        matrix8.set(1, 0, 3.0d);
-        matrix8.set(1, 1, 2.0d);
-
-        assertTrue(matrix7.equals(matrix8));
-
-        // Testing Float branch with two different matrices
-        Matrix<Float> matrix9 = new Matrix<Float>(2, 2);
-        matrix9.set(0, 0, 4.0f);
-        matrix9.set(0, 1, 9.0f);
-        matrix9.set(1, 0, 3.0f);
-        matrix9.set(1, 1, 2.0f);
-
-        Matrix<Float> matrix10 = new Matrix<Float>(2, 2);
-        matrix10.set(0, 0, 1.0f);
-        matrix10.set(0, 1, 2.0f);
-        matrix10.set(1, 0, 5.0f);
-        matrix10.set(1, 1, 6.0f);
-
-        assertFalse(matrix9.equals(matrix10));
     }
 
     /**
@@ -202,6 +182,7 @@ public class MatrixTests {
      */
     @Test
     public void testNumericTypesCompare2() {
+        // Testing Long branch, the second condition in the if statement.
         Matrix<Integer> matrix5 = new Matrix<Integer>(2, 2);
         matrix5.set(0, 0, 4);
         matrix5.set(0, 1, 9);
